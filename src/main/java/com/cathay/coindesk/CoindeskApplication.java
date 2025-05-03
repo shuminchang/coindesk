daa18317fe42bf9dd14f83c86eb67e78876a90ab
shuminchang <magic2313@gmail.com>
2025-05-03 15:52:14 +0800

    init commit

---
+++ src/main/java/com/cathay/coindesk/CoindeskApplication.java
@@ -0,0 +1,13 @@
+package com.cathay.coindesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoindeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoindeskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cathay/coindesk/CoindeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cathay.coindesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoindeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
